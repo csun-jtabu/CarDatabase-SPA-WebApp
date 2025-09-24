3f5060f3c5458f2e1a381cfa3dc154b75a3937e0
Jaztin <77910637+csun-jtabu@users.noreply.github.com>
2025-09-24 16:39:19 -0700

    Fixing CORS error, Applying Angular Material, Making Weather Table

--- src/app/weather/weather.ts
+++ src/app/weather/weather.ts
@@ -1,17 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { WeatherData } from '../weather-data';
+import { MatToolbar } from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-weather',
-  imports: [],
+  imports: [MatToolbar],
   templateUrl: './weather.html',
   styleUrl: './weather.scss'
 })
 export class Weather {
+  forecasts: WeatherData[] = [];
+
   constructor(http: HttpClient) 
   {
-    http.get<WeatherData[]>('https://localhost:7260/weatherforecast').subscribe(result => {});
+    http.get<WeatherData[]>('https://localhost:7260/weatherforecast').subscribe(result => {
+      this.forecasts = result;
+    });
 
   }
 }
